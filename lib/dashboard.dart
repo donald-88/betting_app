c153074f5c768bf1be23b3e101cc1beb4721cab9
McDonald Namba <nambamcdonald@gmail.com>
2022-09-21 12:43:28 +0200

    pulling in data from api

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,39 +1,158 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:betting_app/widgets/myCard.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:http/http.dart' as http;
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  getOddsData() async {
+    var response = await http.get(
+        Uri.https('api.betting-api.com', '1xbet/football/live/all'),
+        headers: {
+          HttpHeaders.authorizationHeader:
+              '58be903e11884db0a97eea600fda6bfe271c174c668d48e4bf56f11b3ac19cd8'
+        });
+
+    var jsonData = jsonDecode(response.body);
+    List<Slip> slips = [];
+
+    for (var s in jsonData) {
+      Slip slip = Slip(s['team1'], s['team2'], s['type']);
+      slips.add(slip);
+    }
+    return slips;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade800,
+      backgroundColor: const Color.fromARGB(255, 41, 41, 41),
       appBar: AppBar(
+        centerTitle: true,
         leading: IconButton(
           onPressed: () {},
-          icon: const Icon(FontAwesomeIcons.bars),
+          splashRadius: 16,
+          icon: const Icon(Iconsax.menu),
           color: Colors.white,
         ),
-        backgroundColor: Colors.grey.shade800,
+        backgroundColor: const Color.fromARGB(255, 41, 41, 41),
         title: const Text(
-          "Betting App",
-          style: TextStyle(color: Colors.white),
+          "Get Money Gang",
+          style: TextStyle(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
-          SizedBox(height: 20),
-          MyCard(),
-          MyCard(),
-          MyCard(),
-          MyCard(),
-          MyCard(),
-          MyCard(),
-          MyCard()
-        ],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              height: 50,
+              decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 61, 61, 61),
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      const Text(
+                        "League",
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      ),
+                      IconButton(
+                        splashRadius: 16,
+                        icon: const Icon(
+                          Iconsax.arrow_down_1,
+                          size: 16,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        "Bet Type",
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      ),
+                      IconButton(
+                        splashRadius: 16,
+                        icon: const Icon(
+                          Iconsax.arrow_down_1,
+                          size: 16,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder<dynamic>(
+                  future: getOddsData(),
+                  builder: (context, snapshot) {
+                    if (snapshot.data == null) {
+                      return const Center(
+                        child: Text('...Loading...'),
+                      );
+                    } else {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            return MyCard(
+                                team1: snapshot.data[index].team1,
+                                team2: snapshot.data[index].team2);
+                          });
+                    }
+                  }),
+            )
+          ],
+        ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color.fromARGB(255, 41, 41, 41),
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Iconsax.home,
+                  color: Colors.white,
+                ),
+                label: ''),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Iconsax.notification,
+                  color: Color.fromARGB(255, 151, 151, 151),
+                ),
+                label: ''),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Iconsax.wallet,
+                  color: Color.fromARGB(255, 151, 151, 151),
+                ),
+                label: '')
+          ]),
     );
   }
+}
+
+class Slip {
+  final String team1, team2, type;
+
+  Slip(this.team1, this.team2, this.type);
 }
--- lib/widgets/myCard.dart
+++ lib/widgets/myCard.dart
@@ -1,48 +1,81 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class MyCard extends StatelessWidget {
-  const MyCard({Key? key}) : super(key: key);
+  final String team1;
+  final String team2;
+
+  const MyCard({required this.team1, required this.team2});
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return Card(
-      color: Colors.grey,
+    return Container(
+      decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 61, 61, 61),
+          borderRadius: BorderRadius.all(Radius.circular(25))),
+      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
       child: Center(
-        child: Container(
-          width: width,
+        child: SizedBox(
           height: 100,
           child: Column(
             children: [
-              Container(
+              SizedBox(
                 height: 60,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [Text("Team1"), Text("VS"), Text("Team2")],
+                  children: [
+                    Flexible(
+                      child: Text(
+                        team1,
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 14),
+                            textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const Text(
+                      "vs",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Flexible(
+                      child: Text(
+                        team2,
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 14),
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  ],
                 ),
               ),
-              Container(
-                width: width,
+              SizedBox(
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      color: Colors.white,
-                      child: Center(child: Text("1")),
-                      width: (width / 3) - 10,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    SizedBox(
+                      child: Center(
+                          child: Text(
+                        "1",
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      )),
                     ),
-                    Container(
-                      color: Colors.white,
-                      child: Center(child: Text("Draw")),
-                      width: (width / 3) - 10,
+                    SizedBox(
+                      child: Center(
+                          child: Text(
+                        "Draw",
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      )),
                     ),
-                    Container(
-                      color: Colors.white,
-                      child: Center(child: Text("2")),
-                      width: (width / 3) - 10,
+                    SizedBox(
+                      child: Center(
+                          child: Text(
+                        "2",
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      )),
                     )
                   ],
                 ),
