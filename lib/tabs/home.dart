b586acdca6113caa11486159a44247ea08349911
McDonald Namba <nambamcdonald@gmail.com>
2023-03-19 14:51:30 +0200

    fixed checkmark on filterchips

--- lib/tabs/home.dart
+++ lib/tabs/home.dart
@@ -13,7 +13,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   List leagues = [
     'EPL',
     'LFP',
@@ -24,9 +23,7 @@ class _HomeState extends State<Home> {
     'UEL',
   ];
 
-  List odds = [
-    1.0,1.1,1.2,1.3
-  ];
+  List odds = [1.0, 1.1, 1.2, 1.3];
 
   List oddsType = [];
 
@@ -39,10 +36,17 @@ class _HomeState extends State<Home> {
     Colors.purple.shade300,
     Colors.orange.shade300
   ];
-  
-  List filterOptions = ['English Premier League', 'Spanish La Liga', 'Bundesliga', 'Italian Serie A', 'ligue 1', 'UEFA Champions League', 'UEFA Europa League'];
+
+  List filterOptions = [
+    'England. Premier League',
+    'Spain. La Liga',
+    'Germany. Bundesliga',
+    'Italy. Serie A',
+    'France. Ligue 1',
+    'UEFA Champions League',
+    'UEFA Europa League',
+  ];
   List selectedFilters = [];
-  
 
   final String league = 'UEFA Europa League';
 
@@ -58,83 +62,100 @@ class _HomeState extends State<Home> {
 
     jsonData.where((element) => element['markets']['totals']);
 
-    List filteredList = jsonData.where((element){
+    List filteredList = jsonData.where((element) {
       return selectedFilters.contains(element['title']);
     }).toList();
-    
+
     return jsonData;
   }
-  
 
   @override
   Widget build(BuildContext context) {
+    print(selectedFilters);
     return SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text("Get Money!!", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
-              const SizedBox(height: 28),
-              SizedBox(
-                height: 40,
-                width: double.infinity,
-                child: ListView.builder(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "Get Money!!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 28),
+            SizedBox(
+              height: 40,
+              width: double.infinity,
+              child: ListView.builder(
                   scrollDirection: Axis.horizontal,
                   itemCount: leagues.length,
-                  itemBuilder: (context, index){
-                  return Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 4),
-                    child: FilterChip(label: Text(leagues[index]),selected: selectedFilters.contains(leagues[index]),selectedColor: Colors.amber,checkmarkColor: Colors.white ,onSelected: (isSelected){
-                      setState(() {
-                        isSelected? selectedFilters.add(filterOptions[index]) : selectedFilters.remove(filterOptions[index]);
-                      });
-                    }));
-                }),
-              ),
-              const SizedBox(height: 8),
-              SizedBox(
-                height: 40,
-                width: double.infinity,
-                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 4),
+                        child: FilterChip(
+                            label: Text(leagues[index]),
+                            selected:
+                                selectedFilters.contains(filterOptions[index]),
+                            selectedColor: Colors.amber,
+                            checkmarkColor: Colors.white,
+                            onSelected: (isSelected) {
+                              setState(() {
+                                isSelected
+                                    ? selectedFilters.add(filterOptions[index])
+                                    : selectedFilters
+                                        .remove(filterOptions[index]);
+                              });
+                            }));
+                  }),
+            ),
+            const SizedBox(height: 8),
+            SizedBox(
+              height: 40,
+              width: double.infinity,
+              child: ListView.builder(
                   scrollDirection: Axis.horizontal,
                   itemCount: odds.length,
-                  itemBuilder: (context, index){
-                  return Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 4),
-                    child: FilterChip(
-                      label: Text('x${odds[index].toString()}'), onSelected: (bool value){}));
-                }),
-              ),
-              const SizedBox(height: 20),
-              Expanded(
-                child: FutureBuilder<dynamic>(
-                    future: getOddsData(),
-                    builder: (context, snapshot) {
-                      if (snapshot.data == null) {
-                        return const Center(
-                          child: SpinKitFoldingCube(color: Colors.white,size: 40),
-                        );
-                      } else {
-                        return ListView.builder(
-                            itemCount: snapshot.data!.length,
-                            itemBuilder: (context, index) {
-                              return MyCard(
-                                team1: snapshot.data[index]['team1'],
-                                team2: snapshot.data[index]['team2'],
-                                score1: snapshot.data[index]['score1'].toString(),
-                                score2: snapshot.data[index]['score2'].toString(),
-                                minute: snapshot.data[index]['minute'].toString(),
-                                odds: '1.0',
-                                color: chipColors[0],
-                              );
-                            });
-                      }
-                    }),
-              )
-            ],
-          ),
+                  itemBuilder: (context, index) {
+                    return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 4),
+                        child: FilterChip(
+                            label: Text('x${odds[index].toString()}'),
+                            onSelected: (bool value) {}));
+                  }),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: FutureBuilder<dynamic>(
+                  future: getOddsData(),
+                  builder: (context, snapshot) {
+                    if (snapshot.data == null) {
+                      return const Center(
+                        child:
+                            SpinKitFoldingCube(color: Colors.white, size: 40),
+                      );
+                    } else {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            return MyCard(
+                              team1: snapshot.data[index]['team1'],
+                              team2: snapshot.data[index]['team2'],
+                              score1: snapshot.data[index]['score1'].toString(),
+                              score2: snapshot.data[index]['score2'].toString(),
+                              minute: snapshot.data[index]['minute'].toString(),
+                              odds: '1.0',
+                              color: chipColors[0],
+                            );
+                          });
+                    }
+                  }),
+            )
+          ],
         ),
-      );
+      ),
+    );
   }
 }
